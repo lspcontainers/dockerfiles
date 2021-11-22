b22257c9a65abf8418e7448cc6de74cdc5ba5225
Emiliano Ruiz Carletti <contact@eruizc.dev>
2021-11-22 02:14:15 -0300

    Update

---
+++ servers/solargraph/docker_entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "hello $@"
+
+# TODO:
+# 1. Create a rvm group and use it
+# 2. Install gems: bundler, solargraph and yard
+# 3. Run `bundle install` to install project dependencies
+# 4. Run `solargraph download-core` to download the core documentation
+# 5. Run `yard gems` to generate documentation for all dependencies
+# 6. Call $@ (mainly `solargraph stdio`)
