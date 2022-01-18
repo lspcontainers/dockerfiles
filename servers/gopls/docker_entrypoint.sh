8422225d6720f50ddf198bfdcc512a18db27e320
Erik Reinert <erik@altf4.email>
2022-01-17 17:45:53 -0800

    feat(gopls): syntac

--- servers/gopls/docker_entrypoint.sh
+++ servers/gopls/docker_entrypoint.sh
@@ -6,5 +6,6 @@ GOPLS_GROUP="$(id -g)"
 
 sudo usermod -u $GOPLS_USER gopls \
   && sudo groupmod -g $GOPLS_GROUP gopls \
+  && sudo chown -R "$GOPLS_USER:$GOPLS_GROUP" /home/gopls \
   && echo "$GOPLS_USER:$GOPLS_GROUP" \
   && exec "gopls"
