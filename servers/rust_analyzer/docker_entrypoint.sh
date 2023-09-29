c9b838c285f2d77629514a20361388774c8a2c81
Philip Deuchler <pbdeuchler@gmail.com>
2023-09-28 23:17:22 -0600

    Remove entrypoint.sh file, move to nightly dockerfile and just install rust-analyzer

--- servers/rust_analyzer/docker_entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -eu pipefail
-
-LSPCONTAINERS_GROUP=$(id -g)
-LSPCONTAINERS_USER=$(id -u)
-
-sudo usermod -u $LSPCONTAINERS_USER lspcontainers \
-    && sudo groupmod -g $LSPCONTAINERS_GROUP lspcontainers || true \
-    && exec "rust-analyzer"
